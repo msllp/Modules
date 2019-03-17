6f57d537da5c8fdf30f4913856a8abf5019d47bc
Mitul Patel <33424960+mitulpatel18@users.noreply.github.com>
2019-03-17 12:27:18 +0530

    Initial commit

---
+++ composer.json.php
@@ -0,0 +1,22 @@
+{
+"name": "msllp/modules",
+"type": "library",
+"license": "MIT",
+"authors": [
+{
+"name": "Million Solutions LLP",
+"email": "dev@millionsllp.com"
+}
+],
+"require": {
+"php": "^7.1.3",
+"fideloper/proxy": "^4.0",
+"laravel/framework": "5.8.*",
+"laravel/tinker": "^1.0",
+"laravel/framework": "5.8.*"
+},
+"autoload": {
+"psr-4": {
+"MS\\Modules\\": "src/Modules"}
+}
+}
